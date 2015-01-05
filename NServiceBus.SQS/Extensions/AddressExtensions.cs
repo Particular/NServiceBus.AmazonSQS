b938f6fdde31836bc2c24c0d47f02a452b73d87f
Adran Hofman <adrian@adrianhofman.com>
2015-01-06 09:44:51 +1100

    Manual merge

--- NServiceBus.SQS/Extensions/AddressExtensions.cs
+++ NServiceBus.SQS/Extensions/AddressExtensions.cs
@@ -4,7 +4,21 @@
     {
         public static string ToSqsQueueName(this Address address)
         {
-            return address.ToString().Replace('@', '-');
+			// SQS queue names can only have alphanumeric characters, hyphens and underscores.
+			// Any other characters will be replaced with a hyphen.
+			var s = address.ToString();
+			for (int i = 0; i<s.Length; ++i)
+			{
+				var c = s[i];
+				if ( !char.IsLetterOrDigit(c) 
+					&& c != '-'
+					&& c != '_')
+				{
+					s = s.Replace(c, '-');
+				}
+
+			}
+			return s;
         }
     }
 }
