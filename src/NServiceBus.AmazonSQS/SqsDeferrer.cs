ce6a25c57fb24e8da5b955096fe4945aa137e7e3
ahofman <adrian@adrianhofman.com>
2015-01-29 08:06:37 +1100

    WIP Sqs Deferrer

---
+++ src/NServiceBus.AmazonSQS/SqsDeferrer.cs
@@ -0,0 +1,37 @@
+﻿namespace NServiceBus.Transports.SQS
+{
+    using System;
+    using Amazon.SQS;
+    using Amazon.SQS.Model;
+    using NServiceBus.Logging;
+    using NServiceBus.Unicast;
+    using NServiceBus.Unicast.Transport;
+
+    internal class SqsDeferrer : IDeferMessages
+    {
+        public SqsQueueSender Sender { get; set; }
+
+        public void Defer(TransportMessage message, SendOptions sendOptions)
+        {
+           /* TimeSpan delayDeliveryBy;
+            if (sendOptions.DelayDeliveryWith.HasValue)
+                delayDeliveryBy = sendOptions.DelayDeliveryWith.Value;
+            else
+            {
+                if (sendOptions.DeliverAt.HasValue)
+                {
+                    delayDeliveryBy = sendOptions.DeliverAt.Value - DateTime.UtcNow;
+                }
+            }
+
+            Sender.*/
+        }
+
+        public void ClearDeferredMessages(string headerKey, string headerValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        static ILog Log = LogManager.GetLogger<SqsDeferrer>();
+    }
+}
