d5e417d2c25a81fd97183783b0c737b4f8cec92a
ahofman <adrian@adrianhofman.com>
2015-03-09 14:34:17 +1100

    Comment, API tidy

--- src/NServiceBus.AmazonSQS/Extensions/SettingsExtensions.cs
+++ src/NServiceBus.AmazonSQS/Extensions/SettingsExtensions.cs
@@ -5,14 +5,20 @@
 
     public static class SettingsExtensions
     {
+        /// <summary>
+        /// Configures the SQS transport to use SQS message delays for deferring messages. 
+        /// If not called, the default is to use a TimeoutManager based deferral.
+        /// </summary>
+        /// <param name="transportExtensions"></param>
+        /// <param name="use">Set to true to use SQS message delays for deferring messages; false otherwise.</param>
         public static void UseSqsDeferral(this TransportExtensions<SqsTransport> transportExtensions, bool use = true)
         {
             transportExtensions.GetSettings().Set("UseSqsDeferral", use);
         }
 
-        public static bool UseSqsDeferral(this ReadOnlySettings settings)
+        static bool UseSqsDeferral(this ReadOnlySettings settings)
         {
-            return settings.Get<bool>("UseSqsDeferral");
+            return settings.GetOrDefault<bool>("UseSqsDeferral");
         }
     }
 }
