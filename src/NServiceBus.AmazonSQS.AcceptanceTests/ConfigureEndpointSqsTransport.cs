55fc2fd0eb401e4bb55293f617a979847241b217
Adran Hofman <adrian@adrianhofman.com>
2017-04-27 20:58:28 +1000

    Acceptance Tests figured out

---
+++ src/NServiceBus.AmazonSQS.AcceptanceTests/ConfigureEndpointSqsTransport.cs
@@ -0,0 +1,19 @@
+﻿using NServiceBus.AcceptanceTesting.Support;
+using System;
+using System.Threading.Tasks;
+
+namespace NServiceBus.AcceptanceTests
+{
+    public class ConfigureEndpointSqsTransport : IConfigureEndpointTestExecution
+    {
+        public Task Cleanup()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Configure(string endpointName, EndpointConfiguration configuration, RunSettings settings, PublisherMetadata publisherMetadata)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/NServiceBus.AmazonSQS.AcceptanceTests/TestSuiteConstraints.cs
@@ -0,0 +1,63 @@
+﻿using NServiceBus.AcceptanceTesting.Support;
+
+namespace NServiceBus.AcceptanceTests
+{
+    public partial class TestSuiteConstraints
+    {
+        public IConfigureEndpointTestExecution PersistenceConfiguration
+        {
+            get
+            {
+                return new ConfigureEndpointInMemoryPersistence();
+            }
+        }
+
+        public bool SupportsCrossQueueTransactions
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public bool SupportsDtc
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public bool SupportsNativeDeferral
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public bool SupportsNativePubSub
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public bool SupportsOutbox
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public IConfigureEndpointTestExecution TransportConfiguration
+        {
+            get
+            {
+                return new ConfigureEndpointSqsTransport();
+            }
+        }
+    }
+}
