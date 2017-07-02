324f00018f5e6abd00241d58dae5fea8b9542945
Sean Feldman <SeanFeldman@users.noreply.github.com>
2017-07-02 04:58:04 -0600

    Acceptance tests cleanup based on test run ID (#91)

---
+++ src/NServiceBus.AmazonSQS.AcceptanceTests/Infrastructure/TestIndependenceSkipBehavior.cs
@@ -0,0 +1,29 @@
+﻿namespace NServiceBus.AcceptanceTests.Infrastructure
+{
+    using System;
+    using System.Threading.Tasks;
+    using NServiceBus.AcceptanceTesting;
+    using NServiceBus.Pipeline;
+
+    class TestIndependenceSkipBehavior : IBehavior<ITransportReceiveContext, ITransportReceiveContext>
+    {
+        string testRunId;
+
+        public TestIndependenceSkipBehavior(ScenarioContext scenarioContext)
+        {
+            testRunId = scenarioContext.TestRunId.ToString();
+        }
+
+        public Task Invoke(ITransportReceiveContext context, Func<ITransportReceiveContext, Task> next)
+        {
+            string runId;
+            if (!context.Message.Headers.TryGetValue("$AcceptanceTesting.TestRunId", out runId) || runId != testRunId)
+            {
+                Console.WriteLine($"Skipping message {context.Message.MessageId} from previous test run");
+                return Task.CompletedTask;
+            }
+
+            return next(context);
+        }
+    }
+}
