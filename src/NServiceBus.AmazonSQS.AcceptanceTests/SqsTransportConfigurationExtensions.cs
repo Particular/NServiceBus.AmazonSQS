702d1fbd9310a659d76cc9f75cce134ee9819461
Adrian Hofman <adrian@adrianhofman.com>
2017-07-09 13:02:21 +1000

    Changes per PR feedback

--- src/NServiceBus.AmazonSQS.AcceptanceTests/SqsTransportConfigurationExtensions.cs
+++ src/NServiceBus.AmazonSQS.AcceptanceTests/SqsTransportConfigurationExtensions.cs
@@ -23,13 +23,12 @@
             if (!string.IsNullOrEmpty(s3BucketName))
                 transportConfiguration.S3BucketForLargeMessages(s3BucketName, "test");
 
-            var nativeDeferralRaw = EnvironmentHelper.GetEnvironmentVariable(NativeDeferralEnvironmentVariableName);
-            var nativeDeferral = true;
-            if (!string.IsNullOrEmpty(nativeDeferralRaw))
-                nativeDeferral = bool.Parse(nativeDeferralRaw);
-
-            if (nativeDeferral)
+            var nativeDeferralRaw = EnvironmentHelper.GetEnvironmentVariable(NativeDeferralEnvironmentVariableName);        
+            var validValue = bool.TryParse(nativeDeferralRaw, out var nativeDeferral);
+            if (validValue && nativeDeferral)
+            {
                 transportConfiguration.NativeDeferral();
+            }
 
             return transportConfiguration;
         }
