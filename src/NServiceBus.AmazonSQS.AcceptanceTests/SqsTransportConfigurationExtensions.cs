1543dd524862b00d895f2886bbb534bc78e5e572
Adrian Hofman <adrian@adrianhofman.com>
2017-07-11 08:01:04 +1000

    Issue 94 at config (#96)
    * Extract the configuration of sqs for testing into a common method. Use this in AT and TT.
    Allow region, s3bucket and native deferral to be specified from environment variables.
    
    * Add documentation on new environment variables
    
    * Changes per PR feedback

---
+++ src/NServiceBus.AmazonSQS.AcceptanceTests/SqsTransportConfigurationExtensions.cs
@@ -0,0 +1,36 @@
+﻿namespace NServiceBus.AmazonSQS.AcceptanceTests
+{
+    using NServiceBus.AcceptanceTests.ScenarioDescriptors;
+
+    public static class SqsTransportConfigurationExtensions
+    {
+
+        const string RegionEnvironmentVariableName = "NServiceBus.AmazonSQS.Region";
+        const string S3BucketEnvironmentVariableName = "NServiceBus.AmazonSQS.S3Bucket";
+        const string NativeDeferralEnvironmentVariableName = "NServiceBus.AmazonSQS.NativeDeferral";
+
+        public static TransportExtensions<SqsTransport> ConfigureSqsTransport(this TransportExtensions<SqsTransport> transportConfiguration, string queueNamePrefix)
+        {
+            var region = EnvironmentHelper.GetEnvironmentVariable(RegionEnvironmentVariableName) ?? "ap-southeast-2";
+
+            transportConfiguration
+                .Region(region)
+                .QueueNamePrefix(queueNamePrefix)
+                .PreTruncateQueueNamesForAcceptanceTests();
+
+            var s3BucketName = EnvironmentHelper.GetEnvironmentVariable(S3BucketEnvironmentVariableName);
+
+            if (!string.IsNullOrEmpty(s3BucketName))
+                transportConfiguration.S3BucketForLargeMessages(s3BucketName, "test");
+
+            var nativeDeferralRaw = EnvironmentHelper.GetEnvironmentVariable(NativeDeferralEnvironmentVariableName);        
+            var validValue = bool.TryParse(nativeDeferralRaw, out var nativeDeferral);
+            if (validValue && nativeDeferral)
+            {
+                transportConfiguration.NativeDeferral();
+            }
+
+            return transportConfiguration;
+        }
+    }
+}
