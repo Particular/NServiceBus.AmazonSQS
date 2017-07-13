a9a64908a6bd9121faaaa0dccb33bc9fb9669619
danielmarbach <daniel.marbach@openplace.net>
2017-07-13 15:31:34 +0200

    Using GitVersion to determine assembly version information

--- src/NServiceBus.AmazonSQS/Properties/AssemblyInfo.cs
+++ src/NServiceBus.AmazonSQS/Properties/AssemblyInfo.cs
@@ -7,8 +7,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: ComVisible(false)]
 [assembly: Guid("79a7efd9-fcdf-4018-a808-3047411ba945")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: InternalsVisibleTo("NServiceBus.AmazonSQS.Tests")]
 [assembly: InternalsVisibleTo("NServiceBus.AmazonSQS.AcceptanceTests")]
 [assembly: InternalsVisibleTo("NServiceBus.AmazonSQS.TransportTests")]
