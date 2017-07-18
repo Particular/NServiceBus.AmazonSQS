b1c2d2ff76178c2a4254cc11a686597f9a523011
danielmarbach <daniel.marbach@openplace.net>
2017-07-18 11:14:16 +0200

    Prevent When_publishing_from_sendonly from running since it is wrong in the core

--- src/NServiceBus.AmazonSQS.AcceptanceTests/ConfigureEndpointSqsTransport.cs
+++ src/NServiceBus.AmazonSQS.AcceptanceTests/ConfigureEndpointSqsTransport.cs
@@ -9,12 +9,13 @@
     {
         public Task Configure(string endpointName, EndpointConfiguration configuration, RunSettings settings, PublisherMetadata publisherMetadata)
         {
+            PreventInconclusiveTestsFromRunning(endpointName);
+
             var transportConfig = configuration.UseTransport<SqsTransport>();
 
             transportConfig.ConfigureSqsTransport(SetupFixture.SqsQueueNamePrefix);
 
             var routingConfig = transportConfig.Routing();
-
             foreach (var publisher in publisherMetadata.Publishers)
             {
                 foreach (var eventType in publisher.Events)
@@ -27,6 +28,7 @@
 
             var recoverability = configuration.Recoverability();
             recoverability.DisableLegacyRetriesSatellite();
+
             return Task.FromResult(0);
         }
 
@@ -34,6 +36,14 @@
         {
             // Queues are cleaned up once, globally, in SetupFixture.
             return Task.FromResult(0);
+        }
+
+        static void PreventInconclusiveTestsFromRunning(string endpointName)
+        {
+            if (endpointName == Conventions.EndpointNamingConvention(typeof(When_publishing_from_sendonly.SendOnlyPublisher)))
+            {
+                Assert.Inconclusive("Test is not using endpoint naming conventions in hardcoded subscription storage. Should be fixed in core vNext.");
+            }
         }
     }
 }
