92b29915d9d4f2b8fae68183eb3883a6eba797ed
Simon Cropp <simon.cropp@gmail.com>
2017-09-15 07:58:39 +1000

    remove redundant code

---
+++ src/Tests/Sending_messages.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NServiceBus;
+using NServiceBus.AmazonSQS;
+using NServiceBus.Extensibility;
+using NUnit.Framework;
+using NServiceBus.Routing;
+using NServiceBus.Settings;
+using NServiceBus.Transport;
+using NServiceBus.Transports.SQS;
+
+[TestFixture]
+public class Sending_messages
+{
+    [Test]
+    public void throws_when_message_is_large_and_no_s3_bucket_configured()
+    {
+        var settings = new SettingsHolder();
+        var transportSettings = new TransportExtensions<SqsTransport>(settings);
+        transportSettings
+            .Region("ap-southeast-2");
+
+        var sut = new MessageDispatcher(new ConnectionConfiguration(settings), null, null, null);
+
+        var stringBuilder = new StringBuilder();
+        while (stringBuilder.Length < 256 * 1024)
+        {
+            stringBuilder.Append("This is a large string. ");
+        }
+
+        var largeOutgoingMessageToSend = new OutgoingMessage("1234",
+            new Dictionary<string, string>(),
+            Encoding.Default.GetBytes(stringBuilder.ToString()));
+
+        var transportOperations = new TransportOperations(
+            new TransportOperation(
+                largeOutgoingMessageToSend,
+                new UnicastAddressTag("address")));
+
+        var transportTransaction = new TransportTransaction();
+        var context = new ContextBag();
+
+        Assert.ThrowsAsync<Exception>(async () => await sut.Dispatch(transportOperations, transportTransaction, context));
+    }
+}
