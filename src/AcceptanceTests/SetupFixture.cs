7cfebe7413631fe2fc9ee4cbb21ef8eb7d3cc135
Simon Cropp <simon.cropp@gmail.com>
2017-10-11 18:04:50 +1100

    Revert "set SqsQueueNamePrefix in static context"
    This reverts commit d8d9604eaffbdd086ef9f4f7691ff33ead686790.

--- src/AcceptanceTests/SetupFixture.cs
+++ src/AcceptanceTests/SetupFixture.cs
@@ -11,13 +11,21 @@
     [SetUpFixture]
     public class SetupFixture
     {
-        // The queue name prefix for the current run of the test suite.
-        // Generate a new queue name prefix for acceptance tests
-        // every time the tests are run.
-        // This is to work around an SQS limitation that prevents
-        // us from deleting then creating a queue with the
-        // same name in a 60 second period.
-        public static string SqsQueueNamePrefix = $"AT{DateTime.Now:yyyyMMddHHmmss}";
+        /// <summary>
+        /// The queue name prefix for the current run of the test suite.
+        /// </summary>
+        public static string SqsQueueNamePrefix { get; private set; }
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            // Generate a new queue name prefix for acceptance tests
+            // every time the tests are run. 
+            // This is to work around an SQS limitation that prevents
+            // us from deleting then creating a queue with the 
+            // same name in a 60 second period.
+            SqsQueueNamePrefix = $"AT{DateTime.Now:yyyyMMddHHmmss}";
+        }
 
         [OneTimeTearDown]
         public async Task OneTimeTearDown()
