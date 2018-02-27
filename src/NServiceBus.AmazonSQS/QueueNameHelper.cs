401a70cb18dde7d47d14f0d4d2ec1ddd04bcfaad
Sean Feldman <feldman.sean@gmail.com>
2018-02-27 16:02:50 -0700

    Update QueueNameHelper to preserve .fifo in the name of delayed delivery queues
    Co-authored-by: Brandon Ording <bording@gmail.com>
    Co-authored-by: Sean Feldman <feldman.sean@gmail.com>

--- src/NServiceBus.AmazonSQS/QueueNameHelper.cs
+++ src/NServiceBus.AmazonSQS/QueueNameHelper.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Linq;
+    using System.Text;
 
     static class QueueNameHelper
     {
@@ -17,30 +18,32 @@
             if (connectionConfiguration.PreTruncateQueueNames && s.Length > 80)
             {
                 var charsToTake = 80 - connectionConfiguration.QueueNamePrefix.Length;
-                s = connectionConfiguration.QueueNamePrefix +
+                s = connectionConfiguration.QueueNamePrefix + 
                     new string(s.Reverse().Take(charsToTake).Reverse().ToArray());
-            }
-
-            // SQS queue names can only have alphanumeric characters, hyphens and underscores.
-            // Any other characters will be replaced with a hyphen.
-            for (var i = 0; i < s.Length; ++i)
-            {
-                var c = s[i];
-                if (!char.IsLetterOrDigit(c)
-                    && c != '-'
-                    && c != '_')
-                {
-                    s = s.Replace(c, '-');
-                }
             }
 
             if (s.Length > 80)
             {
-                throw new Exception(
-                    $"Address {destination} with configured prefix {connectionConfiguration.QueueNamePrefix} is longer than 80 characters and therefore cannot be used to create an SQS queue. Use a shorter queue name.");
+                throw new Exception($"Address {destination} with configured prefix {connectionConfiguration.QueueNamePrefix} is longer than 80 characters and therefore cannot be used to create an SQS queue. Use a shorter queue name.");
             }
 
-            return s;
+            var skipCharacters = s.EndsWith(".fifo") ? 5 : 0;
+            var queueNameBuilder = new StringBuilder(s);
+
+            // SQS queue names can only have alphanumeric characters, hyphens and underscores.
+            // Any other characters will be replaced with a hyphen.
+            for (var i = 0; i < queueNameBuilder.Length - skipCharacters; ++i)
+            {
+                var c = queueNameBuilder[i];
+                if (!char.IsLetterOrDigit(c)
+                    && c != '-'
+                    && c != '_')
+                {
+                    queueNameBuilder[i] = '-';
+                }
+            }
+
+            return queueNameBuilder.ToString();
         }
     }
 }
