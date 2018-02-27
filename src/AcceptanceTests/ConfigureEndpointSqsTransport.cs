ca442cd77c780c2c707232ac332f90cbda2b09ca
Sean Feldman <feldman.sean@gmail.com>
2018-02-27 16:01:59 -0700

    Enable delayed delivery for ATTs by default
    Co-authored-by: Brandon Ording <bording@gmail.com>
    Co-authored-by: Sean Feldman <feldman.sean@gmail.com>

--- src/AcceptanceTests/ConfigureEndpointSqsTransport.cs
+++ src/AcceptanceTests/ConfigureEndpointSqsTransport.cs
@@ -32,6 +32,8 @@
             // Wait for tests longer than the invisibility time to make sure messages are received.
             settings.TestExecutionTimeout = TimeSpan.FromSeconds(40);
 
+            transportConfig.UnrestrictedDurationDelayedDelivery();
+
             return Task.FromResult(0);
         }
 
