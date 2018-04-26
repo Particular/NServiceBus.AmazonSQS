f090b4a4942024fa2fdf19d695e2cf43a94c9682
Bob Langley <bob.langley@particular.net>
2018-04-25 20:58:59 -0700

    Reintroduce TimeToBeReceived and ReplyToAddress properties on TransportMessage for backwards compatibility

---
+++ src/NServiceBus.AmazonSQS.Tests/TransportMessageTests.cs
@@ -0,0 +1,119 @@
+﻿namespace NServiceBus.AmazonSQS.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using DeliveryConstraints;
+    using NUnit.Framework;
+    using Performance.TimeToBeReceived;
+    using Transport;
+
+    [TestFixture]
+    public class TransportMessageTests
+    {
+        static TimeSpan expectedTtbr = TimeSpan.MaxValue.Subtract(TimeSpan.FromHours(1));
+        const string expectedReplyToAddress = "TestReplyToAddress";
+
+        [Test]
+        public void Defaults_TimeToBeReceived_to_TimeSpan_MaxTime_when_DiscardIfNotReceivedBefore_is_not_provided()
+        {
+            var outgoingMessage = new OutgoingMessage(string.Empty, new Dictionary<string, string>(), new byte[0]);
+            var deliveryConstrants = new List<DeliveryConstraint>();
+
+            var transportMessage = new TransportMessage(outgoingMessage, deliveryConstrants);
+
+            Assert.AreEqual(TimeSpan.MaxValue.ToString(), transportMessage.TimeToBeReceived, "TimeToBeReceived is not TimeSpan.MaxValue");
+        }
+
+        [Test]
+        public void Populates_TimeToBeReceived_when_DiscardIfNotReceivedBefore_is_provided()
+        {
+            var outgoingMessage = new OutgoingMessage(string.Empty, new Dictionary<string, string>(), new byte[0]);
+            var deliveryConstrants = new List<DeliveryConstraint>
+            {
+                new DiscardIfNotReceivedBefore(expectedTtbr)
+            };
+
+            var transportMessage = new TransportMessage(outgoingMessage, deliveryConstrants);
+
+            Assert.AreEqual(expectedTtbr.ToString(), transportMessage.TimeToBeReceived, "TimeToBeReceived is not the expected value");
+        }
+
+        [Test]
+        public void Populates_TimeToBeReceived_when_TimeToBeReceived_Header_is_present()
+        {
+            var transportMessage = new TransportMessage();
+            transportMessage.Headers = new Dictionary<string, string>
+            {
+                { TransportHeaders.TimeToBeReceived, expectedTtbr.ToString() }
+            };
+
+            Assert.AreEqual(expectedTtbr.ToString(), transportMessage.TimeToBeReceived, "TimeToBeReceived does not match expected value.");
+        }
+
+        [Test]
+        public void Adds_TimeToBeReceived_Header_when_property_value_is_provided()
+        {
+            var transportMessage = new TransportMessage
+            {
+                TimeToBeReceived = expectedTtbr.ToString()
+            };
+
+            Assert.IsTrue(transportMessage.Headers.ContainsKey(TransportHeaders.TimeToBeReceived), "TimeToBeReceived header is missing");
+            Assert.AreEqual(expectedTtbr.ToString(), transportMessage.Headers[TransportHeaders.TimeToBeReceived], "TimeToBeReceived header does not match expected value.");
+        }
+
+        [Test]
+        public void Does_not_add_TimeToBeReceived_Header_when_property_is_set_to_null()
+        {
+            var transportMessage = new TransportMessage();
+
+            Assert.IsFalse(transportMessage.Headers.ContainsKey(TransportHeaders.TimeToBeReceived), "TimeToBeReceived header was populated");
+        }
+
+        [Test]
+        public void Populates_ReplyToAddress_when_header_is_present()
+        {            
+            var outgoingMessage = new OutgoingMessage(string.Empty, new Dictionary<string, string>
+            {
+                { Headers.ReplyToAddress, expectedReplyToAddress }
+            }, new byte[0]);
+
+            var transportMessage = new TransportMessage(outgoingMessage, new List<DeliveryConstraint>());
+
+            Assert.AreEqual(expectedReplyToAddress, transportMessage.ReplyToAddress, "ReplyToAddress is not the expected value");
+        }
+
+        [Test]
+        public void ReplyToAddress_is_null_when_no_ReplyToAddress_header_is_present()
+        {
+            var outgoingMessage = new OutgoingMessage(string.Empty, new Dictionary<string, string>(), new byte[0]);
+
+            var transportMessage = new TransportMessage(outgoingMessage, new List<DeliveryConstraint>());
+
+            Assert.IsNull(transportMessage.ReplyToAddress, "ReplyToAddress is not null");
+        }
+
+        [Test]
+        public void Adds_ReplyToAddress_Header_when_property_value_is_provided()
+        {
+            var transportMessage = new TransportMessage
+            {
+                ReplyToAddress = expectedReplyToAddress
+            };
+
+            Assert.IsTrue(transportMessage.Headers.ContainsKey(Headers.ReplyToAddress), "ReplyToAddress header is missing");
+            Assert.AreEqual(expectedReplyToAddress, transportMessage.Headers[Headers.ReplyToAddress], "ReplyToAddress header does not match expected value.");
+        }
+
+        [Test]
+        public void Does_not_add_ReplyToAddress_Header_when_property_value_is_set_to_null()
+        {
+            var transportMessage = new TransportMessage
+            {
+                ReplyToAddress = null
+            };
+
+            Assert.IsFalse(transportMessage.Headers.ContainsKey(Headers.ReplyToAddress), "ReplyToAddress header was created");
+        }
+    }
+}
