852f8a6d098415cde83c6cd00e54054c3c969700
Bob Langley <bob.langley@particular.net>
2018-05-11 08:44:49 -0700

    Add Newtonsoft contract resolver to ignore TTBR/ReplyToAddress properties on SqsTransportMessage

---
+++ src/NServiceBus.AmazonSQS/ReducedPayloadContractResolver.cs
@@ -0,0 +1,20 @@
+﻿namespace NServiceBus.AmazonSQS
+{
+    using System.Reflection;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Serialization;
+
+    class ReducedPayloadContractResolver : DefaultContractResolver
+    {
+        protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+        {
+            var property = base.CreateProperty(member, memberSerialization);
+
+            property.ShouldSerialize = instance => property.DeclaringType != typeof(SqsTransportMessage) ||
+                                                   (property.PropertyName != nameof(SqsTransportMessage.TimeToBeReceived) &&
+                                                    property.PropertyName != nameof(SqsTransportMessage.ReplyToAddress));
+
+            return property;
+        }
+    }
+}
