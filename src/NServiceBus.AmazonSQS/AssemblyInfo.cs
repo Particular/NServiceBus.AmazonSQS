33e1307a7ce4673945be229ddd3b4701f3d28762
Brandon Ording <bording@gmail.com>
2018-12-18 15:37:54 -0500

    Move to Particular.Packaging

--- src/NServiceBus.AmazonSQS/AssemblyInfo.cs
+++ src/NServiceBus.AmazonSQS/AssemblyInfo.cs
@@ -2,9 +2,5 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("NServiceBus.AmazonSQS")]
-[assembly: AssemblyCopyright("Copyright NServiceBus. All rights reserved")]
-[assembly: AssemblyProduct("NServiceBus.AmazonSQS")]
-[assembly: AssemblyCompany("NServiceBus Ltd.")]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
