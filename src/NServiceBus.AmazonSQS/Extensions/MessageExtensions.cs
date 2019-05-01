11978eb6ef8b8135da9aa4e3d1906dbd75d01579
danielmarbach <daniel.marbach@openplace.net>
2019-05-01 14:15:40 +0200

    Switch from BufferedStream to MemoryStream for S3 messages

--- src/NServiceBus.AmazonSQS/Extensions/MessageExtensions.cs
+++ src/NServiceBus.AmazonSQS/Extensions/MessageExtensions.cs
@@ -10,38 +10,24 @@
     static class MessageExtensions
     {
         public static async Task<byte[]> RetrieveBody(this TransportMessage transportMessage,
-            IAmazonS3 amazonS3,
+            IAmazonS3 s3Client,
             TransportConfiguration transportConfiguration,
             CancellationToken cancellationToken)
         {
-            byte[] body;
-
             if (string.IsNullOrEmpty(transportMessage.S3BodyKey))
             {
-                body = Convert.FromBase64String(transportMessage.Body);
+                return Convert.FromBase64String(transportMessage.Body);
             }
-            else
+
+            var s3GetResponse = await s3Client.GetObjectAsync(transportConfiguration.S3BucketForLargeMessages,
+                transportMessage.S3BodyKey,
+                cancellationToken).ConfigureAwait(false);
+
+            using (var memoryStream = new MemoryStream())
             {
-                var s3GetResponse = await amazonS3.GetObjectAsync(transportConfiguration.S3BucketForLargeMessages,
-                    transportMessage.S3BodyKey,
-                    cancellationToken).ConfigureAwait(false);
-
-                body = new byte[s3GetResponse.ResponseStream.Length];
-                using (var bufferedStream = new BufferedStream(s3GetResponse.ResponseStream))
-                {
-                    int count;
-                    var transferred = 0;
-                    const int maxChunkSize = 8 * 1024;
-                    var bytesToRead = Math.Min(maxChunkSize, body.Length - transferred);
-                    while ((count = await bufferedStream.ReadAsync(body, transferred, bytesToRead, cancellationToken).ConfigureAwait(false)) > 0)
-                    {
-                        transferred += count;
-                        bytesToRead = Math.Min(maxChunkSize, body.Length - transferred);
-                    }
-                }
+                await s3GetResponse.ResponseStream.CopyToAsync(memoryStream).ConfigureAwait(false);
+                return memoryStream.ToArray();
             }
-
-            return body;
         }
 
         public static DateTime GetSentDateTime(this Message message, TimeSpan clockOffset)
