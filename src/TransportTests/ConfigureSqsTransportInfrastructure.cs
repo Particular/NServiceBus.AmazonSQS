2a90b4c22d0e8340347862587b8d9a28bd9d45f9
mauroservienti <mauro.servienti@gmail.com>
2020-03-26 11:53:14 +0100

    Configure transport infrastructure to add MessageMetadataRegistry

--- src/TransportTests/ConfigureSqsTransportInfrastructure.cs
+++ src/TransportTests/ConfigureSqsTransportInfrastructure.cs
@@ -1,8 +1,12 @@
-﻿using NServiceBus.TransportTests;
+﻿using System;
+using System.Globalization;
+using System.Reflection;
+using NServiceBus.TransportTests;
 using System.Threading.Tasks;
 using NServiceBus.Settings;
 using NServiceBus;
 using NServiceBus.AmazonSQS.AcceptanceTests;
+using NServiceBus.Unicast.Messages;
 
 public class ConfigureSqsTransportInfrastructure : IConfigureTransportInfrastructure
 {
@@ -17,6 +21,15 @@ public class ConfigureSqsTransportInfrastructure : IConfigureTransportInfrastruc
         var sqsConfig = new TransportExtensions<SqsTransport>(settings);
         
         sqsConfig.ConfigureSqsTransport(SetupFixture.SqsQueueNamePrefix);
+
+        bool IsMessageType(Type t) => true;
+        var messageMetadataRegistry = (MessageMetadataRegistry)Activator.CreateInstance(
+            type:typeof(MessageMetadataRegistry),
+            bindingAttr: BindingFlags.NonPublic | BindingFlags.Instance,
+            binder: null, 
+            args: new object[] {(Func<Type, bool>)IsMessageType},
+            culture: CultureInfo.InvariantCulture);
+        settings.Set(messageMetadataRegistry);
 
         return new TransportConfigurationResult
         {
