4ead3e6b230d5c941c7b4045823598e9906cfeed
danielmarbach <daniel.marbach@openplace.net>
2020-03-26 13:59:30 +0100

    Share creationg of metadata registry as source

--- src/TransportTests/ConfigureSqsTransportInfrastructure.cs
+++ src/TransportTests/ConfigureSqsTransportInfrastructure.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Globalization;
-using System.Reflection;
-using NServiceBus.TransportTests;
-using System.Threading.Tasks;
-using NServiceBus.Settings;
+﻿using System.Threading.Tasks;
 using NServiceBus;
 using NServiceBus.AmazonSQS.AcceptanceTests;
-using NServiceBus.Unicast.Messages;
+using NServiceBus.AmazonSQS.Tests;
+using NServiceBus.Settings;
+using NServiceBus.TransportTests;
 
 public class ConfigureSqsTransportInfrastructure : IConfigureTransportInfrastructure
 {
@@ -14,22 +11,15 @@ public class ConfigureSqsTransportInfrastructure : IConfigureTransportInfrastruc
     {
         return Task.FromResult(0);
     }
-    
+
     public TransportConfigurationResult Configure(SettingsHolder settings, TransportTransactionMode transactionMode)
     {
         var sqsTransport = new SqsTransport();
         var sqsConfig = new TransportExtensions<SqsTransport>(settings);
-        
+
         sqsConfig.ConfigureSqsTransport(SetupFixture.SqsQueueNamePrefix);
 
-        bool IsMessageType(Type t) => true;
-        var messageMetadataRegistry = (MessageMetadataRegistry)Activator.CreateInstance(
-            type:typeof(MessageMetadataRegistry),
-            bindingAttr: BindingFlags.NonPublic | BindingFlags.Instance,
-            binder: null, 
-            args: new object[] {(Func<Type, bool>)IsMessageType},
-            culture: CultureInfo.InvariantCulture);
-        settings.Set(messageMetadataRegistry);
+        settings.SetupMessageMetadataRegistry();
 
         return new TransportConfigurationResult
         {
