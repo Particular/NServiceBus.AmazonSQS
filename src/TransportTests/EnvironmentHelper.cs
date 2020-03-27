113c77c4344a87728dc3a81c63d4a8997c8d2410
danielmarbach <daniel.marbach@openplace.net>
2020-03-27 12:15:50 +0100

    Bring EnvironmentHelper into transport tests to avoid having to reference atts

--- src/TransportTests/EnvironmentHelper.cs
+++ src/TransportTests/EnvironmentHelper.cs
@@ -1,7 +1,19 @@
-namespace TransportTests
+namespace NServiceBus.AcceptanceTests.ScenarioDescriptors
 {
-    public class EnvironmentHelper
+    using System;
+
+    class EnvironmentHelper
     {
-        
+        public static string GetEnvironmentVariable(string variable)
+        {
+            var candidate = Environment.GetEnvironmentVariable(variable, EnvironmentVariableTarget.User);
+
+            if (string.IsNullOrWhiteSpace(candidate))
+            {
+                return Environment.GetEnvironmentVariable(variable);
+            }
+
+            return candidate;
+        }
     }
 }
