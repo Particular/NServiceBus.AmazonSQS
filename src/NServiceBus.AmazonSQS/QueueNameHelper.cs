697d69b821ec880c8c4746102733c34581c51173
danielmarbach <daniel.marbach@openplace.net>
2020-03-27 11:22:11 +0100

    Remove partial

--- src/NServiceBus.AmazonSQS/QueueNameHelper.cs
+++ src/NServiceBus.AmazonSQS/QueueNameHelper.cs
@@ -4,7 +4,7 @@
     using System.Linq;
     using System.Text;
 
-    static partial class QueueNameHelper
+    static class QueueNameHelper
     {
         public static string GetSqsQueueName(string destination, TransportConfiguration transportConfiguration)
         {
