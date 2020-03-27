c7250bc59978b3567c670f9b32cdaf149a66d46f
danielmarbach <daniel.marbach@openplace.net>
2020-03-27 14:10:37 +0100

    Simplify cleanup logic

--- src/Tests/Cleanup.cs
+++ src/Tests/Cleanup.cs
@@ -39,10 +39,59 @@ namespace NServiceBus.AmazonSQS.Tests
 
         [Test]
         [Explicit]
+        public async Task DeleteAllSubscriptions()
+        {
+            await DeleteAllSubscriptionsWithPrefix(snsClient, "AT");
+            await DeleteAllSubscriptionsWithPrefix(snsClient, "TT");            
+        }
+
+        [Test]
+        [Explicit]
         public async Task DeleteAllTopicsUsedForTests()
         {
             await DeleteAllTopicsWithPrefix(snsClient, "AT");
             await DeleteAllTopicsWithPrefix(snsClient, "TT");
+        }
+        
+        [Test]
+        [Explicit]
+        public async Task DeleteAllResourcesWithPrefix()
+        {
+            await DeleteAllResourcesWithPrefix(sqsClient, snsClient, "AT");
+            await DeleteAllResourcesWithPrefix(sqsClient, snsClient, "TT");
+        }
+
+        public static Task DeleteAllResourcesWithPrefix(IAmazonSQS sqsClient, IAmazonSimpleNotificationService snsClient, string namePrefix)
+        {
+            return Task.WhenAll(DeleteAllQueuesWithPrefix(sqsClient, namePrefix), DeleteAllTopicsWithPrefix(snsClient, namePrefix), DeleteAllSubscriptionsWithPrefix(snsClient, namePrefix));
+        }
+
+        public static async Task DeleteAllSubscriptionsWithPrefix(IAmazonSimpleNotificationService snsClient, string topicNamePrefix)
+        {
+            ListSubscriptionsResponse subscriptions;
+            do
+            {
+                subscriptions = await snsClient.ListSubscriptionsAsync();
+                var deletionTasks = new List<Task>(subscriptions.Subscriptions.Count);
+                deletionTasks.AddRange(subscriptions.Subscriptions.Select(async subscription =>
+                {
+                    if (!subscription.TopicArn.Contains($":{topicNamePrefix}"))
+                    {
+                        return;
+                    }
+
+                    try
+                    {
+                        await snsClient.UnsubscribeAsync(subscription.SubscriptionArn).ConfigureAwait(false);
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine($"Unable to delete subscription '{subscription.SubscriptionArn}' '{subscription.TopicArn}'");
+                    }
+                }));
+
+                await Task.WhenAll(deletionTasks).ConfigureAwait(false);
+            } while (subscriptions.NextToken != null && subscriptions.Subscriptions.Count > 0);
         }
 
         public static async Task DeleteAllTopicsWithPrefix(IAmazonSimpleNotificationService snsClient, string topicNamePrefix)
@@ -58,27 +107,7 @@ namespace NServiceBus.AmazonSQS.Tests
                     {
                         return;
                     }
-
-                    ListSubscriptionsByTopicResponse subscriptions = null; 
-                    do
-                    {
-                        subscriptions = await snsClient.ListSubscriptionsByTopicAsync(topic.TopicArn, subscriptions?.NextToken).ConfigureAwait(false);
-                        var subscriptionDeletionTasks = new List<Task>(subscriptions.Subscriptions.Count);
-                        subscriptionDeletionTasks.AddRange(subscriptions.Subscriptions.Select(async subscription =>
-                        {
-                            try
-                            {
-                                await snsClient.UnsubscribeAsync(subscription.SubscriptionArn).ConfigureAwait(false);
-                            }
-                            catch (Exception)
-                            {
-                                Console.WriteLine($"Unable to delete subscription '{subscription.SubscriptionArn}' '{topic.TopicArn}'");
-                            }
-                        }));
-
-                        await Task.WhenAll(subscriptionDeletionTasks).ConfigureAwait(false);
-                    } while (subscriptions.NextToken != null && subscriptions.Subscriptions.Count > 0);
-
+                    
                     try
                     {
                         await snsClient.DeleteTopicAsync(topic.TopicArn).ConfigureAwait(false);
