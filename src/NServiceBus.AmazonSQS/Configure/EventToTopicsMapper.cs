0ac2faf21d6221cd91c496a0a2fa8b9aa586bfe7
mauroservienti <mauro.servienti@gmail.com>
2020-03-30 15:58:19 +0200

    Custom mapping api

---
+++ src/NServiceBus.AmazonSQS/Configure/EventToTopicsMapper.cs
@@ -0,0 +1,29 @@
+﻿namespace NServiceBus
+{
+    using System;
+
+    /// <summary>
+    /// The event to topics mapper used to configure custom mapping between a message type
+    /// and one or more topics in SNS.
+    /// </summary>
+    public class EventToTopicsMapper
+    {
+        internal EventToTopicsMapper( Type eventType, EventToTopicsMappings mappings)
+        {
+            this.eventType = eventType;
+            this.mappings = mappings;
+        }
+
+        /// <summary>
+        /// The list of topics names to map to. Topics will be created if non-existent.
+        /// </summary>
+        public void ToTopics(params string[] topicsNames)
+        {
+            Guard.AgainstNull(nameof(topicsNames), topicsNames);
+            mappings.Add(eventType, topicsNames);
+        }
+
+        readonly Type eventType;
+        readonly EventToTopicsMappings mappings;
+    }
+}
---
+++ src/NServiceBus.AmazonSQS/Configure/EventToTopicsMappings.cs
@@ -0,0 +1,38 @@
+﻿namespace NServiceBus
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class EventToTopicsMappings
+    {
+        public void Add(Type eventType, string[] topicsNames)
+        {
+            if (!eventsToTopicsMappings.TryGetValue(eventType, out var mapping))
+            {
+                mapping = new HashSet<string>();
+                eventsToTopicsMappings.Add(eventType, mapping);
+            }
+
+            foreach (var topicName in topicsNames)
+            {
+                if (!mapping.Contains(topicName))
+                {
+                    mapping.Add(topicName);
+                }
+            }
+        }
+
+        public bool HasMappingsFor(Type eventType)
+        {
+            return eventsToTopicsMappings.ContainsKey(eventType);
+        }
+
+        public string[] GetMappedTopicsNames(Type eventType)
+        {
+            return eventsToTopicsMappings[eventType].ToArray();
+        }
+
+        Dictionary<Type, HashSet<string>> eventsToTopicsMappings = new Dictionary<Type, HashSet<string>>();
+    }
+}
