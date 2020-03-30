ba5904a152caa6159f7413a75f46a2044adb3c86
Yves Goeleven <yves@goeleven.com>
2020-03-30 14:53:04 +0200

    added an ATT for the topic name generator

---
+++ src/AcceptanceTests/NativePubSub/When_customizing_topic_name_generation.cs
@@ -0,0 +1,70 @@
+﻿namespace NServiceBus.AcceptanceTests.NativePubSub
+{
+    using System;
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using AcceptanceTesting.Customization;
+    using EndpointTemplates;
+    using NUnit.Framework;
+
+    public class When_customizing_topic_name_generation : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public async Task It_can_subscribe_for_event_published_on_custom_topic()
+        {
+            var context = await Scenario.Define<Context>()
+                .WithEndpoint<CustomizedPublisher>(b => b.When(c => c.Subscribed, (session, ctx) => session.Publish(new MyEvent())))
+                .WithEndpoint<CustomizedSubscriber>(b => b.When(async (session, ctx) => {
+                    await session.Subscribe<MyEvent>();
+                    ctx.Subscribed = true;
+                }))
+                .Done(c => c.GotTheEvent)
+                .Run(TimeSpan.FromSeconds(30));
+
+            Assert.True(context.GotTheEvent);
+        }
+
+        public class Context : ScenarioContext
+        {
+            public bool GotTheEvent { get; set; }
+            public bool Subscribed { get; set; }
+        }
+
+        public class CustomizedPublisher : EndpointConfigurationBuilder
+        {
+            public CustomizedPublisher()
+            {
+                EndpointSetup<DefaultPublisher>(c =>
+                {
+                    c.ConfigureSqsTransport().TopicNameGenerator((eventType, prefix) => prefix + "-shared-topic");
+                });
+            }
+        }
+
+        public class CustomizedSubscriber : EndpointConfigurationBuilder
+        {
+            public CustomizedSubscriber()
+            {
+                EndpointSetup<DefaultServer>(c =>
+                {
+                    c.ConfigureSqsTransport().TopicNameGenerator((eventType, prefix) => prefix + "-shared-topic");
+                });
+            }
+
+            public class MyEventHandler : IHandleMessages<MyEvent>
+            {
+                public Context Context { get; set; }
+
+                public Task Handle(MyEvent @event, IMessageHandlerContext context)
+                {
+                    Context.GotTheEvent = true;
+                    return Task.FromResult(0);
+                }
+            }
+        }
+
+        public class MyEvent : IEvent
+        {
+        }
+    }
+}
