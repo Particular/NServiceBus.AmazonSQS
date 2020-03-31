3bba9aa8ee305f6d23e344004494030806bfaec0
danielmarbach <daniel.marbach@openplace.net>
2020-03-31 14:36:16 +0200

    Slightly better coverage for queue cache

--- src/Tests/QueueCacheTests.cs
+++ src/Tests/QueueCacheTests.cs
@@ -1,14 +1,50 @@
 ﻿namespace NServiceBus.AmazonSQS.Tests
 {
     using System;
-    using NServiceBus;
-    using AmazonSQS;
-    using Settings;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
     using NUnit.Framework;
+    using Settings;
 
     [TestFixture]
     public class QueueCacheTests
     {
+        [Test]
+        [TestCase("really-really-really-really-really-really-really-really-really-really-really-really-really-really-really-really", "")]
+        [TestCase("PREFIXreally-really-really-really-really-really-really-really-really-really-really-really-really-really-really-really", "PREFIX")]
+        public void ThrowsWhenLongerThanEightyChars(string destination, string queueNamePrefix)
+        {
+            var settings = new SettingsHolder();
+            settings.Set(SettingsKeys.QueueNamePrefix, queueNamePrefix);
+
+            var configuration = new TransportConfiguration(settings);
+
+            var cache = new QueueCache(null, configuration);
+
+            var exception = Assert.Throws<Exception>(() => cache.GetPhysicalQueueName(destination));
+            Assert.That(exception.Message, Contains.Substring("is longer than 80 characters"));
+        }
+
+        [Test]
+        [TestCase("really-really-really-really-really-really-really-really-really-really-really-really-really-really-really-really", "", "lly-really-really-really-really-really-really-really-really-really-really-really")]
+        [TestCase("PREFIXreally-really-really-really-really-really-really-really-really-really-really-really-really-really-really-really", "PREFIX", "PREFIXally-really-really-really-really-really-really-really-really-really-really")]
+        public void DoesNotThrowWithPretruncation(string destination, string queueNamePrefix, string expected)
+        {
+            var settings = new SettingsHolder();
+            settings.Set(SettingsKeys.PreTruncateQueueNames, true);
+            settings.Set(SettingsKeys.QueueNamePrefix, queueNamePrefix);
+
+            var configuration = new TransportConfiguration(settings);
+
+            var cache = new QueueCache(null, configuration);
+
+            var result = cache.GetPhysicalQueueName(destination);
+            var resultIdempotent = cache.GetPhysicalQueueName(result);
+
+            Assert.AreEqual(expected, result);
+            Assert.AreEqual(expected, resultIdempotent);
+        }
+
         [Test]
         [TestCase("destination-delay.fifo", "destination-delay.fifo")]
         [TestCase("destination-delay.blurb", "destination-delay-blurb")]
@@ -18,8 +54,13 @@
         {
             var configuration = new TransportConfiguration(new SettingsHolder());
 
-            var result = QueueCache.GetSqsQueueName(destination, configuration);
+            var cache = new QueueCache(null, configuration);
+
+            var result = cache.GetPhysicalQueueName(destination);
+            var resultIdempotent = cache.GetPhysicalQueueName(result);
+
             Assert.AreEqual(expected, result);
+            Assert.AreEqual(expected, resultIdempotent);
         }
 
         [Test]
@@ -35,9 +76,63 @@
 
             var configuration = new TransportConfiguration(settings);
 
-            var result = QueueCache.GetSqsQueueName(destination, configuration);
-            Console.WriteLine(result);
+            var cache = new QueueCache(null, configuration);
+
+            var result = cache.GetPhysicalQueueName(destination);
+            var resultIdempotent = cache.GetPhysicalQueueName(result);
+
             Assert.AreEqual(expected, result);
+            Assert.AreEqual(expected, resultIdempotent);
+        }
+
+        [Test]
+        [TestCase("destination-1", "destination-1")]
+        [TestCase("destination.1", "destination-1")]
+        [TestCase("destination!1", "destination-1")]
+        [TestCase("destination?1", "destination-1")]
+        [TestCase("destination@1", "destination-1")]
+        [TestCase("destination$1", "destination-1")]
+        [TestCase("destination%1", "destination-1")]
+        [TestCase("destination^1", "destination-1")]
+        [TestCase("destination&1", "destination-1")]
+        [TestCase("destination*1", "destination-1")]
+        [TestCase("destination_1", "destination_1")]
+        public void ReplacesNonDigitsWithDash(string destination, string expected)
+        {
+            var settings = new SettingsHolder();
+
+            var configuration = new TransportConfiguration(settings);
+
+            var cache = new QueueCache(null, configuration);
+
+            var result = cache.GetPhysicalQueueName(destination);
+            var resultIdempotent = cache.GetPhysicalQueueName(result);
+
+            Assert.AreEqual(expected, result);
+            Assert.AreEqual(expected, resultIdempotent);
+        }
+
+        [Test]
+        public async Task GetQueueUrl_caches()
+        {
+            var settings = new SettingsHolder();
+            settings.Set(SettingsKeys.QueueNamePrefix, "PREFIX");
+            
+            var configuration = new TransportConfiguration(settings);
+            var sqsClient = new MockSqsClient();
+            
+            var cache = new QueueCache(sqsClient, configuration);
+
+            await cache.GetQueueUrl("fakeQueueName");
+
+            var requestsSent = new List<string>(sqsClient.QueueUrlRequestsSent);
+            
+            sqsClient.QueueUrlRequestsSent.Clear();
+            
+            await cache.GetQueueUrl("fakeQueueName");
+            
+            Assert.IsEmpty(sqsClient.QueueUrlRequestsSent);
+            CollectionAssert.AreEqual(new List<string> { "PREFIXfakeQueueName" }, requestsSent);
         }
     }
 }
