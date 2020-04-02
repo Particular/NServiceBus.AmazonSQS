cf9b913bf664a0c128714142d164faa742ef80fc
mauroservienti <mauro.servienti@gmail.com>
2020-04-02 14:35:03 +0200

    More mapping configuration options

---
+++ src/NServiceBus.AmazonSQS/Configure/EventToEventMappings.cs
@@ -0,0 +1,31 @@
+﻿namespace NServiceBus
+{
+    using System;
+    using System.Collections.Generic;
+
+    class EventToEventMappings
+    {
+        public void Add(Type subscribedEvent, Type concreteEventType)
+        {
+            if (!eventsToEventsMappings.TryGetValue(subscribedEvent, out var mapping))
+            {
+                mapping = new HashSet<Type>();
+                eventsToEventsMappings.Add(subscribedEvent, mapping);
+            }
+
+            mapping.Add(concreteEventType);
+        }
+
+        public bool HasMappingsFor(Type eventType)
+        {
+            return eventsToEventsMappings.ContainsKey(eventType);
+        }
+
+        public IEnumerable<Type> GetMappedTypes(Type eventType)
+        {
+            return eventsToEventsMappings[eventType];
+        }
+
+        Dictionary<Type, HashSet<Type>> eventsToEventsMappings = new Dictionary<Type, HashSet<Type>>();
+    }
+}
