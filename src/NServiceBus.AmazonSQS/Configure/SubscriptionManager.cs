55b5bca60b876d4903248e75afeb062d4a0f9059
Daniel Marbach <daniel.marbach@openplace.net>
2020-04-07 12:05:28 +0200

    Remove policy fetching

--- src/NServiceBus.AmazonSQS/Configure/SubscriptionManager.cs
+++ src/NServiceBus.AmazonSQS/Configure/SubscriptionManager.cs
@@ -4,7 +4,6 @@ namespace NServiceBus
     using System.Collections.Concurrent;
     using System.Threading;
     using System.Threading.Tasks;
-    using Amazon.Auth.AccessControlPolicy;
     using Amazon.SimpleNotificationService;
     using Amazon.SimpleNotificationService.Model;
     using Amazon.SQS;
@@ -186,9 +185,6 @@ namespace NServiceBus
                     AttributeValue = "true"
                 }).ConfigureAwait(false);
                 Logger.Debug($"Set raw delivery for subscription with arn '{createdSubscription}' for '{topicName}' with arn '{topic.TopicArn}' for queue '{queueName}");
-
-                var queueAttributes = await sqsClient.GetAttributesAsync(queueUrl).ConfigureAwait(false);
-                Logger.Debug($"Created policy '{Policy.FromJson(queueAttributes["Policy"]).ToJson(prettyPrint: true)}");
             }
             finally
             {
