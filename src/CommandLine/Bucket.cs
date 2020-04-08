141b6a1d45b2e132218da21f40675cac064f712e
Yves Goeleven <yves@goeleven.com>
2020-04-08 12:39:09 +0200

    experimenting with the cli options and arguments

---
+++ src/CommandLine/Bucket.cs
@@ -0,0 +1,18 @@
+﻿namespace NServiceBus.Transport.SQS.CommandLine
+{
+    using System;
+    using System.Threading.Tasks;
+    using Amazon.S3;
+    using McMaster.Extensions.CommandLineUtils;
+
+    static class Bucket
+    {
+        public static async Task Create(IAmazonS3 s3, CommandArgument name)
+        {
+            await Console.Out.WriteLineAsync("Delay delivery queue created");
+
+            await Task.CompletedTask;
+        }
+    }
+
+}
