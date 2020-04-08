47f6eb339a025f06b2ac98d6f397a9b2813d45d5
Yves Goeleven <yves@goeleven.com>
2020-04-08 12:39:09 +0200

    experimenting with the cli options and arguments

--- src/CommandLine/CommandRunner.cs
+++ src/CommandLine/CommandRunner.cs
@@ -2,20 +2,28 @@
 {
     using System;
     using System.Threading.Tasks;
+    using Amazon;
+    using Amazon.S3;
+    using Amazon.SimpleNotificationService;
+    using Amazon.SQS;
     using McMaster.Extensions.CommandLineUtils;
 
     static class CommandRunner
     {
-     /*   public static async Task Run(CommandOption connectionString, Func<ManagementClient, Task> func)
+        public static async Task Run(CommandOption accessKey, CommandOption secret, CommandOption region, Func<IAmazonSQS, IAmazonSimpleNotificationService, IAmazonS3, Task> func)
         {
-            var connectionStringToUse = connectionString.HasValue() ? connectionString.Value() : Environment.GetEnvironmentVariable(EnvironmentVariableName);
+            var accessKeyToUse = accessKey.HasValue() ? accessKey.Value() : Environment.GetEnvironmentVariable(AccessKeyId);
+            var secretToUse = secret.HasValue() ? secret.Value() : Environment.GetEnvironmentVariable(SecretAccessKey);
+            var regionToUse = region.HasValue() ? region.Value() : Environment.GetEnvironmentVariable(Region);
 
-            var client = new ManagementClient(connectionStringToUse);
+            var regionEndpoint = RegionEndpoint.GetBySystemName(regionToUse);
 
-            await func(client);
+            var sqs = new AmazonSQSClient(accessKeyToUse, secretToUse, regionEndpoint);
+            var sns = new AmazonSimpleNotificationServiceClient(accessKeyToUse, secretToUse, regionEndpoint);
+            var s3 = new AmazonS3Client(accessKeyToUse, secretToUse, regionEndpoint);
 
-            await client.CloseAsync();
-        }*/
+            await func(sqs, sns, s3);           
+        }
 
         public const string AccessKeyId = "AWS_ACCESS_KEY_ID";
         public const string Region = "AWS_REGION";
