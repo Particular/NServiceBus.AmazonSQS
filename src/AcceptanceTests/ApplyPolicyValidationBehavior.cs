e69ffa2d7ec6e0540e0ca3046e8b9f7c28ef85ed
danielmarbach <daniel.marbach@openplace.net>
2020-04-10 08:27:11 +0200

    More cleanup

--- src/AcceptanceTests/ApplyPolicyValidationBehavior.cs
+++ src/AcceptanceTests/ApplyPolicyValidationBehavior.cs
@@ -3,13 +3,12 @@ namespace NServiceBus.AcceptanceTests
     using System;
     using System.Threading.Tasks;
     using NServiceBus.Pipeline;
-    using Transports.SQS;
 
     class ApplyPolicyValidationBehavior : Behavior<IOutgoingPublishContext>
     {
         public override Task Invoke(IOutgoingPublishContext context, Func<Task> next)
         {
-            context.Extensions.Set(ValidateSubscriptionDestinationPolicies.Instance);
+            context.Extensions.RequireSubscriptionDestinationPolicyValidation();
 
             return next();
         }
