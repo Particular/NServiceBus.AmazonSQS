15c970daf748a02875f1e162ecd34121497998c7
danielmarbach <daniel.marbach@openplace.net>
2020-04-10 07:32:49 +0200

    Proper naming, larger interval and also throw if no policy available

--- src/NServiceBus.AmazonSQS/ValidDeliveryPolicies.cs
+++ src/NServiceBus.AmazonSQS/ValidDeliveryPolicies.cs
@@ -1,6 +1,12 @@
 namespace NServiceBus.Transports.SQS
 {
+    // doesn't help to make this a struct since it gets boxed anyway by the extension bag
     class ValidDeliveryPolicies
     {
+        ValidDeliveryPolicies()
+        {
+        }
+
+        public static readonly ValidDeliveryPolicies Instance = new ValidDeliveryPolicies();
     }
 }
