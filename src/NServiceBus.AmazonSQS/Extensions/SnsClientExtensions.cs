7e979a5b8ca24f87ce6f4dd9eafe7e702125ff24
danielmarbach <daniel.marbach@openplace.net>
2020-04-11 00:50:04 +0200

    Cleanup

--- src/NServiceBus.AmazonSQS/Extensions/SnsClientExtensions.cs
+++ src/NServiceBus.AmazonSQS/Extensions/SnsClientExtensions.cs
@@ -1,10 +1,7 @@
 namespace NServiceBus.AmazonSQS
 {
     using System;
-    using System.Linq;
     using System.Threading.Tasks;
-    using Amazon.Auth.AccessControlPolicy;
-    using Amazon.Auth.AccessControlPolicy.ActionIdentifiers;
     using Amazon.SimpleNotificationService;
     using Amazon.SimpleNotificationService.Model;
     using Unicast.Messages;
@@ -51,52 +48,6 @@ namespace NServiceBus.AmazonSQS
             } while (upToAHundredSubscriptions.NextToken != null && upToAHundredSubscriptions.Subscriptions.Count > 0);
 
             return null;
-        }
-
-        internal static void AddSQSPermission(Policy policy, string topicArn, string sqsQueueArn)
-        {
-            var statement = new Statement(Statement.StatementEffect.Allow);
-            statement.Actions.Add(SQSActionIdentifiers.SendMessage);
-            statement.Resources.Add(new Resource(sqsQueueArn));
-            statement.Conditions.Add(ConditionFactory.NewSourceArnCondition(topicArn));
-            statement.Principals.Add(new Principal("*"));
-            policy.Statements.Add(statement);
-        }
-
-        internal static bool HasSQSPermission(Policy policy, string topicArn, string sqsQueueArn)
-        {
-            foreach (var statement in policy.Statements)
-            {
-                // See if the statement contains the topic as a resource
-                var containsResource = false;
-                foreach (var resource in statement.Resources)
-                {
-                    if (resource.Id.Equals(sqsQueueArn))
-                    {
-                        containsResource = true;
-                        break;
-                    }
-                }
-
-                // If queue found as the resource see if the condition is for this topic
-                if (containsResource)
-                {
-                    foreach (var condition in statement.Conditions)
-                    {
-                        if ((string.Equals(condition.Type, ConditionFactory.StringComparisonType.StringLike.ToString(), StringComparison.OrdinalIgnoreCase) ||
-                                string.Equals(condition.Type, ConditionFactory.StringComparisonType.StringEquals.ToString(), StringComparison.OrdinalIgnoreCase) ||
-                                string.Equals(condition.Type, ConditionFactory.ArnComparisonType.ArnEquals.ToString(), StringComparison.OrdinalIgnoreCase) ||
-                                string.Equals(condition.Type, ConditionFactory.ArnComparisonType.ArnLike.ToString(), StringComparison.OrdinalIgnoreCase)) &&
-                            string.Equals(condition.ConditionKey, ConditionFactory.SOURCE_ARN_CONDITION_KEY, StringComparison.OrdinalIgnoreCase) &&
-                            condition.Values.Contains(topicArn))
-                        {
-                            return true;
-                        }
-                    }
-                }
-            }
-
-            return false;
         }
     }
 }
