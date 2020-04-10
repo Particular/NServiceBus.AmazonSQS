15c90cb414a57c14895af69eca26ff69438dc973
mauroservienti <mauro.servienti@gmail.com>
2020-04-10 15:17:07 +0200

    Make sure test runs are independent

---
+++ src/AcceptanceTests/TestIndependenceMutator.cs
@@ -0,0 +1,22 @@
+﻿namespace NServiceBus.AmazonSQS.AcceptanceTests
+{
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using MessageMutator;
+
+    class TestIndependenceMutator : IMutateOutgoingTransportMessages
+    {
+        readonly string testRunId;
+
+        public TestIndependenceMutator(ScenarioContext scenarioContext)
+        {
+            testRunId = scenarioContext.TestRunId.ToString();
+        }
+
+        public Task MutateOutgoing(MutateOutgoingTransportMessageContext context)
+        {
+            context.OutgoingHeaders["$AcceptanceTesting.TestRunId"] = testRunId;
+            return Task.FromResult(0);
+        }
+    }
+}
---
+++ src/AcceptanceTests/TestIndependenceSkipBehavior.cs
@@ -0,0 +1,28 @@
+﻿namespace NServiceBus.AmazonSQS.AcceptanceTests
+{
+    using System;
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using Pipeline;
+
+    class TestIndependenceSkipBehavior : IBehavior<ITransportReceiveContext, ITransportReceiveContext>
+    {
+        readonly string testRunId;
+
+        public TestIndependenceSkipBehavior(ScenarioContext scenarioContext)
+        {
+            testRunId = scenarioContext.TestRunId.ToString();
+        }
+
+        public Task Invoke(ITransportReceiveContext context, Func<ITransportReceiveContext, Task> next)
+        {
+            if (!context.Message.Headers.TryGetValue("$AcceptanceTesting.TestRunId", out var runId) || runId != testRunId)
+            {
+                Console.WriteLine($"Skipping message {context.Message.MessageId} from previous test run");
+                return Task.FromResult(0);
+            }
+
+            return next(context);
+        }
+    }
+}
