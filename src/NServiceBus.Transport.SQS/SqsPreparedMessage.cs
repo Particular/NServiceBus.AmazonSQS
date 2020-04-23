feabff5a1e7d187f293ed1521d25b99ef5a7b989
danielmarbach <daniel.marbach@openplace.net>
2020-04-23 19:02:54 +0200

    Cleanup

--- src/NServiceBus.Transport.SQS/SqsPreparedMessage.cs
+++ src/NServiceBus.Transport.SQS/SqsPreparedMessage.cs
@@ -8,9 +8,9 @@
     {
         public override string MessageId
         {
-            get =>  MessageAttributes.ContainsKey(Headers.MessageId) ? MessageAttributes[Headers.MessageId].StringValue : null;
+            get => MessageAttributes.ContainsKey(Headers.MessageId) ? MessageAttributes[Headers.MessageId].StringValue : null;
             set =>
-                // because message attributes are part of the content size restriction we want to prevent message size from changing thus we add it 
+                // because message attributes are part of the content size restriction we want to prevent message size from changing thus we add it
                 // for native delayed deliver as well even though the information is slightly redundant (MessageId is assigned to MessageDeduplicationId for example)
                 MessageAttributes[Headers.MessageId] = new MessageAttributeValue
                 {
@@ -18,13 +18,14 @@
                     DataType = "String"
                 };
         }
+
         public string OriginalDestination { get; set; }
         public string QueueUrl { get; set; }
         public int DelaySeconds { get; set; }
         public Dictionary<string, MessageAttributeValue> MessageAttributes { get; } = new Dictionary<string, MessageAttributeValue>();
         public string MessageGroupId { get; set; }
         public string MessageDeduplicationId { get; set; }
-        
+
         protected override long CalculateAttributesSize()
         {
             var size = 0L;
@@ -40,9 +41,9 @@
                 {
                     stringValuesSum += x?.Length ?? 0;
                 }
-        
+
                 size += stringValuesSum;
-        
+
                 try
                 {
                     size += attributeValue.BinaryValue?.Length ?? 0;
@@ -51,7 +52,7 @@
                 {
                     // if we can't determine the length we ignore it for now
                 }
-        
+
                 var binaryValuesSum = 0L;
                 // ReSharper disable once ForeachCanBeConvertedToQueryUsingAnotherGetEnumerator
                 foreach (var x in attributeValue.BinaryListValues)
@@ -65,10 +66,10 @@
                         // if we can't determine the length we ignore it for now
                     }
                 }
-        
+
                 size += binaryValuesSum;
             }
-        
+
             return size;
         }
     }
