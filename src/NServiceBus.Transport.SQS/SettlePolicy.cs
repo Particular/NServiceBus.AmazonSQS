3480e2543191953048a89bead42d47ae4068eb1e
Szymon Pobiega <szymon.pobiega@gmail.com>
2020-08-26 08:31:00 +0200

    Fix inspections

--- src/NServiceBus.Transport.SQS/SettlePolicy.cs
+++ src/NServiceBus.Transport.SQS/SettlePolicy.cs
@@ -3,7 +3,6 @@ namespace NServiceBus.Transport.SQS
     using System;
     using System.Reflection;
     using System.Threading.Tasks;
-    using Configure;
     using Features;
 
     class SettlePolicy : Feature
