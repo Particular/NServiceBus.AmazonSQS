605f075cb9648ec2bbf8ba2dca260cf7f502e997
Sean Feldman <feldman.sean@gmail.com>
2020-12-08 01:03:19 -0700

    Fix API Approvals

--- src/NServiceBus.AmazonSQS.Tests/API/APIApprovals.cs
+++ src/NServiceBus.AmazonSQS.Tests/API/APIApprovals.cs
@@ -9,7 +9,7 @@ public class APIApprovals
     [Test]
     public void ApproveSqsTransport()
     {
-        var publicApi = ApiGenerator.GeneratePublicApi(typeof(SqsTransport).Assembly, excludeAttributes: new[] { "System.Runtime.Versioning.TargetFrameworkAttribute" });
+        var publicApi = ApiGenerator.GeneratePublicApi(typeof(SqsTransport).Assembly, excludeAttributes: new[] { "System.Runtime.Versioning.TargetFrameworkAttribute", "System.Reflection.AssemblyMetadataAttribute" });
         Approver.Verify(publicApi);
     }
 }
