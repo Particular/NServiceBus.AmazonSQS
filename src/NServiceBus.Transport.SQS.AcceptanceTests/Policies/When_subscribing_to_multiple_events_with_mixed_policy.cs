c8be9dc090df8eb3bfbeb0fe7a763bbc1d072ed1
danielmarbach <daniel.marbach@openplace.net>
2021-01-13 08:39:41 +0100

    Extensive ATT coverage for all scenarios

---
+++ src/NServiceBus.Transport.SQS.AcceptanceTests/Policies/When_subscribing_to_multiple_events_with_mixed_policy.cs
@@ -0,0 +1,106 @@
+﻿namespace NServiceBus.AcceptanceTests.Policies
+{
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using EndpointTemplates;
+    using NUnit.Framework;
+    using C;
+    using D;
+
+    public class When_subscribing_to_multiple_events_with_mixed_policy : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public async Task Should_deliver_events()
+        {
+            var context = await Scenario.Define<Context>()
+                .WithEndpoint<Publisher>(b => b.When(async session =>
+                {
+                    await session.Publish(new MyEvent());
+                    await session.Publish(new MyOtherEvent());
+                }))
+                .WithEndpoint<Subscriber>(b =>
+                {
+                    b.CustomConfig(c =>
+                    {
+                        var policies = c.ConfigureSqsTransport().Policies();
+                        policies.AddNamespaceCondition("NServiceBus.AcceptanceTests.Policies.E");
+                        policies.AddNamespaceCondition("NServiceBus.AcceptanceTests.Policies.F");
+                        policies.AddAccountCondition();
+                        policies.AddTopicNamePrefixCondition();
+                    });
+                })
+                .Done(c => c.GotMyOtherEvent)
+                .Run();
+
+            Assert.IsTrue(context.GotMyEvent);
+            Assert.IsTrue(context.GotMyOtherEvent);
+        }
+
+        public class Context : ScenarioContext
+        {
+            public bool GotMyEvent { get; set; }
+            public bool GotMyOtherEvent { get; set; }
+        }
+
+        public class Publisher : EndpointConfigurationBuilder
+        {
+            public Publisher()
+            {
+                EndpointSetup<DefaultPublisher>();
+            }
+        }
+
+        public class Subscriber : EndpointConfigurationBuilder
+        {
+            public Subscriber()
+            {
+                EndpointSetup<DefaultServer>();
+            }
+
+            public class MyEventHandler : IHandleMessages<MyEvent>
+            {
+                Context testContext;
+
+                public MyEventHandler(Context testContext)
+                {
+                    this.testContext = testContext;
+                }
+
+                public Task Handle(MyEvent message, IMessageHandlerContext context)
+                {
+                    testContext.GotMyEvent = true;
+                    return Task.CompletedTask;
+                }
+            }
+
+            public class MyEventOtherHandler : IHandleMessages<MyEvent>
+            {
+                Context testContext;
+
+                public MyEventOtherHandler(Context testContext)
+                {
+                    this.testContext = testContext;
+                }
+
+                public Task Handle(MyEvent message, IMessageHandlerContext context)
+                {
+                    testContext.GotMyOtherEvent = true;
+                    return Task.CompletedTask;
+                }
+            }
+        }
+    }
+}
+
+namespace NServiceBus.AcceptanceTests.Policies.E
+{
+    public class MyEvent : IEvent
+    {
+    }
+}
+namespace NServiceBus.AcceptanceTests.Policies.F
+{
+    public class MyOtherEvent : IEvent
+    {
+    }
+}
