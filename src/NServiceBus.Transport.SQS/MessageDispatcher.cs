cf3a0e92d7bd7f6939a081c61792cf321ef7aeb3
Mauro Servienti <mauro.servienti@gmail.com>
2021-07-16 10:28:42 +0200

    Explain why the code hits the SNS API

--- src/NServiceBus.Transport.SQS/MessageDispatcher.cs
+++ src/NServiceBus.Transport.SQS/MessageDispatcher.cs
@@ -245,6 +245,13 @@
         {
             var unicastTransportOperation = transportOperation as UnicastTransportOperation;
 
+            // The following check is required by the message-driven pub/sub hybrid mode in Core
+            // to allow endpoints to migrate from message-driven pub/sub to native pub/sub
+            // If the message we're trying to dispatch is a unicast message with a `Publish` intent
+            // but the subscriber is also subscribed via SNS we don't want to dispatch the message twice
+            // the subscriber will receive it via SNS and not via a unicast send.
+            // We can improve the situation a bit by caching the information and thus reduce the amount of times we hit the SNS API.
+            // We need to think abut what happens in case the destination endpoint unsubscribes from the event.
             // these conditions are carefully chosen to only execute the code if really necessary
             if (unicastTransportOperation != null
                 && messageIdsOfMulticastedEvents.Contains(unicastTransportOperation.Message.MessageId)
