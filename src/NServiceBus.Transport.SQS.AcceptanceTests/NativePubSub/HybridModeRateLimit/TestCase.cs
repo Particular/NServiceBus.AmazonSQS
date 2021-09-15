72203c9990b853d557481315b6226b01d4f55435
Mauro Servienti <mauro.servienti@gmail.com>
2021-09-15 10:14:35 +0200

    Define rate exceed failing test cases
    Co-Authored-By: Tomasz Masternak <1092707+tmasternak@users.noreply.github.com>
    Co-Authored-By: Dennis van der Stelt <dvdstelt@gmail.com>

---
+++ src/NServiceBus.Transport.SQS.AcceptanceTests/NativePubSub/HybridModeRateLimit/TestCase.cs
@@ -0,0 +1,28 @@
+namespace NServiceBus.AcceptanceTests.NativePubSub.HybridModeRateLimit
+{
+    using System;
+
+    public class TestCase
+    {
+        public TestCase(int sequence) => Sequence = sequence;
+
+        public int NumberOfEvents { get; internal set; }
+        public TimeSpan? TestExecutionTimeout { get; internal set; }
+        public int MessageVisibilityTimeout { get; internal set; } = DefaultMessageVisibilityTimeout;
+        public TimeSpan SubscriptionsCacheTTL { get; internal set; } = DefaultSubscriptionCacheTTL;
+        public TimeSpan NotFoundTopicsCacheTTL { get; internal set; } = DefaultTopicCacheTTL;
+        public int Sequence { get; }
+
+        public override string ToString() => $"#{Sequence}, " +
+            $"{nameof(NumberOfEvents)}: {NumberOfEvents}, " +
+            $"{nameof(MessageVisibilityTimeout)}: {(MessageVisibilityTimeout == DefaultMessageVisibilityTimeout ? "default" : MessageVisibilityTimeout.ToString())}, " +
+            $"{nameof(TestExecutionTimeout)}: {TestExecutionTimeout?.ToString() ?? "default"}, " +
+            $"{nameof(SubscriptionsCacheTTL)}: {(SubscriptionsCacheTTL == DefaultSubscriptionCacheTTL ? "default" : SubscriptionsCacheTTL.ToString())}, " +
+            $"{nameof(NotFoundTopicsCacheTTL)}: {(NotFoundTopicsCacheTTL == DefaultTopicCacheTTL ? "default" : NotFoundTopicsCacheTTL.ToString())}";
+
+        static TimeSpan DefaultSubscriptionCacheTTL = TimeSpan.FromSeconds(5);
+        static TimeSpan DefaultTopicCacheTTL = TimeSpan.FromSeconds(5);
+        static int DefaultMessageVisibilityTimeout = 30;
+
+    }
+}
