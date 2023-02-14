2bd3d541565a69afe40642ef92abad5ef3e232bb
Jo Palac <j_palac@hotmail.com>
2023-02-14 16:08:25 +1000

    create queues when SetupInfrastructure flag set

--- src/NServiceBus.Transport.SQS/Configure/SqsTransport.cs
+++ src/NServiceBus.Transport.SQS/Configure/SqsTransport.cs
@@ -227,12 +227,17 @@
             var topicCache = new TopicCache(SnsClient, hostSettings.CoreSettings, eventToTopicsMappings, eventToEventsMappings, topicNameGenerator, topicNamePrefix);
             var infra = new SqsTransportInfrastructure(this, hostSettings, receivers, SqsClient, SnsClient, QueueCache, topicCache, S3, Policies, QueueDelayTime, topicNamePrefix, EnableV1CompatibilityMode, DoNotWrapOutgoingMessages);
 
-            var queueCreator = new QueueCreator(SqsClient, QueueCache, S3, maxTimeToLive, QueueDelayTime);
+            if (hostSettings.SetupInfrastructure)
+            {
+                var queueCreator = new QueueCreator(SqsClient, QueueCache, S3, maxTimeToLive, QueueDelayTime);
 
-            var createQueueTasks = sendingAddresses.Select(x => queueCreator.CreateQueueIfNecessary(x, false))
-                .Concat(infra.Receivers.Values.Select(x => queueCreator.CreateQueueIfNecessary(x.ReceiveAddress, true))).ToArray();
+                var createQueueTasks = sendingAddresses.Select(x => queueCreator.CreateQueueIfNecessary(x, false))
+                    .Concat(infra.Receivers.Values.Select(x => queueCreator.CreateQueueIfNecessary(x.ReceiveAddress, true))).ToArray();
 
-            await Task.WhenAll(createQueueTasks).ConfigureAwait(false);
+                await Task.WhenAll(createQueueTasks).ConfigureAwait(false);
+
+                //NOTE do we also need to do the subscriptions here??
+            }
 
             return infra;
         }
