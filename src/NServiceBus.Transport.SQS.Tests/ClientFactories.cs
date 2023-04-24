3d941ff3798e4aa8b5d7284cd8a0fea55692a63b
Daniel Marbach <daniel.marbach@nservicebus.com>
2023-04-24 15:51:51 +0200

    Remove HttpClientCache hack since the issue has been addressed in Version 3.7.106.15 and higher of Core (#2129)

--- src/NServiceBus.Transport.SQS.Tests/ClientFactories.cs
+++ src/NServiceBus.Transport.SQS.Tests/ClientFactories.cs
@@ -1,4 +1,6 @@
-﻿namespace NServiceBus.Transport.SQS.Tests
+﻿#nullable enable
+
+namespace NServiceBus.Transport.SQS.Tests
 {
     using System;
     using Amazon.Runtime;
@@ -8,44 +10,28 @@
 
     public static class ClientFactories
     {
-        public static IAmazonSQS CreateSqsClient(Action<AmazonSQSConfig> configure = default)
+        public static IAmazonSQS CreateSqsClient(Action<AmazonSQSConfig>? configure = default)
         {
             var credentials = new EnvironmentVariablesAWSCredentials();
-            var config = Create<AmazonSQSConfig>();
+            var config = new AmazonSQSConfig();
             configure?.Invoke(config);
             return new AmazonSQSClient(credentials, config);
         }
 
-        public static IAmazonSimpleNotificationService CreateSnsClient(Action<AmazonSimpleNotificationServiceConfig> configure = default)
+        public static IAmazonSimpleNotificationService CreateSnsClient(Action<AmazonSimpleNotificationServiceConfig>? configure = default)
         {
             var credentials = new EnvironmentVariablesAWSCredentials();
-            var config = Create<AmazonSimpleNotificationServiceConfig>();
+            var config = new AmazonSimpleNotificationServiceConfig();
             configure?.Invoke(config);
             return new AmazonSimpleNotificationServiceClient(credentials, config);
         }
 
-        public static IAmazonS3 CreateS3Client(Action<AmazonS3Config> configure = default)
+        public static IAmazonS3 CreateS3Client(Action<AmazonS3Config>? configure = default)
         {
             var credentials = new EnvironmentVariablesAWSCredentials();
-            var config = Create<AmazonS3Config>();
+            var config = new AmazonS3Config();
             configure?.Invoke(config);
             return new AmazonS3Client(credentials, config);
-        }
-
-        // Can be removed once https://github.com/aws/aws-sdk-net/issues/1929 is addressed by the team
-        // setting the cache size to 1 will significantly improve the throughput on non-windows OSS while
-        // windows had already 1 as the default.
-        // There might be other occurrences of setting this setting explicitly in the code base. Make sure to remove them
-        // consistently once the issue is addressed. 
-        static TConfig Create<TConfig>()
-            where TConfig : ClientConfig, new()
-        {
-#if NET
-            var config = new TConfig { HttpClientCacheSize = 1 };
-#else
-            var config = new TConfig();
-#endif
-            return config;
         }
     }
 }
