213c4d33be2b211f88d81a8a2c4370f03f7b345c
Ramon Smits <ramon.smits@gmail.com>
2024-07-08 14:15:10 +0200

    🛠️ Moved obsolete attribute to avoid compiler warning

--- src/NServiceBus.Transport.SQS/MessageDispatcherProperties.cs
+++ src/NServiceBus.Transport.SQS/MessageDispatcherProperties.cs
@@ -3,14 +3,18 @@ namespace NServiceBus.Transport.SQS.Experimental;
 
 using System;
 
-[Obsolete("This is an experimental setting")]
+/// <summary>
+/// TransportOperation extensions
+/// </summary>
 public static class TransportOperationExt
 {
     internal static string FlatHeadersKey = "FlatHeaders";
 
     /// <summary>
-    /// Unsupported setting to not wrap headers into a single property for sending native messages
+    /// Experimental setting to not wrap headers into a single property for sending native messages
     /// </summary>
+    [Obsolete("This is experimental")]
+    [DoNotWarnAboutObsoleteUsage]
     public static void UseFlatHeaders(this TransportOperation instance)
     {
         instance.Properties[FlatHeadersKey] = bool.TrueString;
