291c8cc3a0c8b2eb9e462aab6b09cb35e5b107c3
Daniel Marbach <daniel.marbach@openplace.net>
2024-12-02 12:19:35 +0100

    When not wrapping fallback to base64 encoding in case invalid SQS chars are being used (#2646)
    * When not wrapping fallback to base64 encoding in case invalid SQS chars are being used (most straightforward impl ignoring perf)
    
    * If only I would understand bools
    
    * Better test
    
    * Better assert
    
    * Documentation
    
    * Acceptance test
    
    * Copy body
    
    * Cleanup
    
    * Header value with invalid chars works too
    
    ---------
    
    Co-authored-by: danielmarbach <danielmarbach@users.noreply.github.com>

--- src/NServiceBus.Transport.SQS.TransportTests.DoNotWrapOutgoingMessages/Sending_messages_with_invalid_sqs_chars.cs
+++ src/NServiceBus.Transport.SQS.TransportTests.DoNotWrapOutgoingMessages/Sending_messages_with_invalid_sqs_chars.cs
@@ -1,38 +1,47 @@
 ﻿namespace NServiceBus.TransportTests
 {
-    using System;
     using System.Collections.Generic;
     using System.Threading.Tasks;
     using NUnit.Framework;
     using Transport;
 
-    public class Sending_metrics_messages : NServiceBusTransportTest
+    public class Sending_messages_with_invalid_sqs_chars : NServiceBusTransportTest
     {
         [TestCase(TransportTransactionMode.None)]
         [TestCase(TransportTransactionMode.ReceiveOnly)]
-        public async Task Should_not_fail_when_using_do_not_wrap(
+        public async Task Should_receive_message(
             TransportTransactionMode transactionMode)
         {
             var messageProcessed = CreateTaskCompletionSource<MessageContext>();
+            byte[] copyOfTheBody = null;
 
             await StartPump(
-                (context, _) => messageProcessed.SetCompleted(context),
+                (context, _) =>
+                {
+                    // This is crucial due to internal buffer pooling in SQS transport
+                    copyOfTheBody = context.Body.ToArray();
+                    return messageProcessed.SetCompleted(context);
+                },
                 (_, __) => Task.FromResult(ErrorHandleResult.Handled),
                 TransportTransactionMode.None);
 
             var headers = new Dictionary<string, string>
             {
-                { Transport.SQS.Constants.MetricsMessageMetricTypeHeaderKey, "doesn't matter" },
-                { Headers.ContentType, Transport.SQS.Constants.MetricsMessageContentTypeHeaderValue }
+                { "SomeHeader", "header value with invalid chars: \0" },
             };
-            var body = Guid.NewGuid().ToByteArray();
+
+            var body = "body with invalid chars: \0"u8.ToArray();
 
             await SendMessage(InputQueueName, headers, body: body);
 
             var messageContext = await messageProcessed.Task;
 
             Assert.That(messageContext.Headers, Is.Not.Empty);
-            Assert.That(messageContext.Headers, Is.SupersetOf(headers));
+            Assert.Multiple(() =>
+            {
+                Assert.That(messageContext.Headers, Is.SupersetOf(headers));
+                Assert.That(copyOfTheBody, Is.EquivalentTo(body));
+            });
         }
     }
 }
--- src/NServiceBus.Transport.SQS/Constants.cs
+++
@@ -1,7 +0,0 @@
-namespace NServiceBus.Transport.SQS;
-
-class Constants
-{
-    public const string MetricsMessageContentTypeHeaderValue = "TaggedLongValueWriterOccurrence";
-    public const string MetricsMessageMetricTypeHeaderKey = "NServiceBus.Metric.Type";
-}
