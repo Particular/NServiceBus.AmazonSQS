b7053d157f3602f1e5f5bd89e4e6c04e52fd1c34
Daniel Marbach <danielmarbach@users.noreply.github.com>
2025-03-31 15:51:11 +0200

    Simplify tests

--- src/NServiceBus.Transport.SQS.Tests/RenewalTimeCalculationTests.cs
+++ src/NServiceBus.Transport.SQS.Tests/RenewalTimeCalculationTests.cs
@@ -7,32 +7,26 @@ using NUnit.Framework;
 [TestFixture]
 public class RenewalTimeCalculationTests
 {
-    [Test]
-    public void Calculate_WhenRemainingTimeIsOneSecond_ReturnsHalfSecondDelay()
+    [Theory]
+    [
+        TestCase(0, 0),
+        TestCase(1, 0.5),
+        TestCase(20, 10),
+        TestCase(160, 150)
+    ]
+    public void Should_calculate_correctly(int renewalTime, double expected)
     {
         var fixedTime = new DateTimeOffset(2023, 1, 1, 0, 0, 0, TimeSpan.Zero);
         var fakeTimeProvider = new FakeTimeProvider(fixedTime);
-        var visibilityTimeExpiresOn = fixedTime.AddSeconds(1);
+        var visibilityTimeExpiresOn = fixedTime.AddSeconds(renewalTime);
 
         var result = RenewalTimeCalculation.Calculate(visibilityTimeExpiresOn, fakeTimeProvider);
 
-        Assert.That(result, Is.EqualTo(TimeSpan.FromSeconds(0.5)));
+        Assert.That(result, Is.EqualTo(TimeSpan.FromSeconds(expected)));
     }
 
     [Test]
-    public void Calculate_WhenRemainingTimeIsTwentySeconds_ReturnsTenSecondsDelay()
-    {
-        var fixedTime = new DateTimeOffset(2023, 1, 1, 0, 0, 0, TimeSpan.Zero);
-        var fakeTimeProvider = new FakeTimeProvider(fixedTime);
-        var visibilityTimeExpiresOn = fixedTime.AddSeconds(20);
-
-        var result = RenewalTimeCalculation.Calculate(visibilityTimeExpiresOn, fakeTimeProvider);
-
-        Assert.That(result, Is.EqualTo(TimeSpan.FromSeconds(10)));
-    }
-
-    [Test]
-    public void Calculate_WhenRemainingTimeIsLessThan400Milliseconds_ReturnsZeroDelay()
+    public void Should_return_zero_delay_for_remaining_time_smaller_than_minimum_threshold()
     {
         var fixedTime = new DateTimeOffset(2023, 1, 1, 0, 0, 0, TimeSpan.Zero);
         var fakeTimeProvider = new FakeTimeProvider(fixedTime);
@@ -41,17 +35,5 @@ public class RenewalTimeCalculationTests
         var result = RenewalTimeCalculation.Calculate(visibilityTimeExpiresOn, fakeTimeProvider);
 
         Assert.That(result, Is.EqualTo(TimeSpan.Zero));
-    }
-
-    [Test]
-    public void Calculate_WhenRemainingTimeIsOneHundredSixtySeconds_ReturnsOneHundredFiftySecondsDelay()
-    {
-        var fixedTime = new DateTimeOffset(2023, 1, 1, 0, 0, 0, TimeSpan.Zero);
-        var fakeTimeProvider = new FakeTimeProvider(fixedTime);
-        var visibilityTimeExpiresOn = fixedTime.AddSeconds(160);
-
-        var result = RenewalTimeCalculation.Calculate(visibilityTimeExpiresOn, fakeTimeProvider);
-
-        Assert.That(result, Is.EqualTo(TimeSpan.FromSeconds(150)));
     }
 }
