0910136272f475d7a507befc48a4df28775f1c25
Mauro Servienti <mauro.servienti@gmail.com>
2025-04-07 10:01:58 +0200

    Allow overriding the message ID

--- src/NServiceBus.Transport.SQS/InputQueuePump.cs
+++ src/NServiceBus.Transport.SQS/InputQueuePump.cs
@@ -443,7 +443,7 @@ namespace NServiceBus.Transport.SQS
             foreach (var receivedMessageMessageAttribute in receivedMessage.MessageAttributes)
             {
                 // TODO is there a better way to achieve the same result? This seems fragile
-                if (TransportHeaders.AllTransportHeaders.Contains(receivedMessageMessageAttribute.Key) || receivedMessageMessageAttribute.Key == Headers.MessageId)
+                if (TransportHeaders.AllTransportHeaders.Contains(receivedMessageMessageAttribute.Key))
                 {
                     continue;
                 }
