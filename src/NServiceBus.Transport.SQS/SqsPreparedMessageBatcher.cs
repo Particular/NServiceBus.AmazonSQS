2040349fab8bbbf4591b5aab482be2fa1b6baa34
Daniel Marbach <danielmarbach@users.noreply.github.com>
2025-04-09 16:49:03 +0200

    Fix batcher logic

--- src/NServiceBus.Transport.SQS/SqsPreparedMessageBatcher.cs
+++ src/NServiceBus.Transport.SQS/SqsPreparedMessageBatcher.cs
@@ -11,53 +11,55 @@ static class SqsPreparedMessageBatcher
     public static IReadOnlyList<SqsBatchEntry> Batch(IEnumerable<SqsPreparedMessage> preparedMessages)
     {
         var allBatches = new List<SqsBatchEntry>();
-        var currentDestinationBatches = new Dictionary<string, SqsPreparedMessage>(TransportConstraints.MaximumItemsInBatch);
+        var currentBatch = new Dictionary<string, SqsPreparedMessage>(TransportConstraints.MaximumItemsInBatch);
 
+        // Group messages by destination to ensure batches only contain messages for the same queue
         var groupByDestination = preparedMessages.GroupBy(m => m.QueueUrl, StringComparer.Ordinal);
-        foreach (var group in groupByDestination)
+
+        foreach (var destinationGroup in groupByDestination)
         {
-            SqsPreparedMessage? firstMessage = null;
-            var payloadSize = 0L;
-            foreach (var message in group)
+            SqsPreparedMessage? referenceMessage = null;
+            var currentBatchSize = 0L;
+
+            foreach (var message in destinationGroup)
             {
-                firstMessage ??= message;
+                referenceMessage ??= message;
+                var messageSize = message.Size; // Size calculation is assumed to be done previously
 
-                // Assumes the size was already calculated by the dispatcher
-                var size = message.Size;
-                payloadSize += size;
-
-                if (payloadSize > TransportConstraints.MaximumMessageSize)
+                // Check if this message would push the batch over the size limit
+                if (currentBatchSize + messageSize > TransportConstraints.MaximumMessageSize)
                 {
-                    // if the first message is already over the limit then it has not yet been added to current destination batches
-                    // so the only thing we do is resetting the payload size and assuming the messages will be added below
-                    // without rechecking the payload size using the service limits enforcing the message cannot be sent
-                    if (currentDestinationBatches.Count > 0)
+                    // Finalize current batch if it has any messages
+                    if (currentBatch.Count > 0)
                     {
-                        allBatches.Add(message.ToBatchRequest(currentDestinationBatches));
-                        currentDestinationBatches.Clear();
+                        allBatches.Add(referenceMessage.ToBatchRequest(currentBatch));
+                        currentBatch.Clear();
                     }
-                    payloadSize = size;
+                    currentBatchSize = messageSize;
                 }
-
-                // we don't have to recheck payload size here because the support layer checks that a request can always fit 256 KB size limit
-                // we can't take MessageId because batch request ID can only contain alphanumeric characters, hyphen and underscores, message id could be overloaded
-                currentDestinationBatches.Add(Guid.NewGuid().ToString(), message);
-
-                var currentCount = currentDestinationBatches.Count;
-                if (currentCount != TransportConstraints.MaximumItemsInBatch)
+                else
                 {
-                    continue;
+                    // Message will fit within the current batch
+                    currentBatchSize += messageSize;
                 }
 
-                allBatches.Add(message.ToBatchRequest(currentDestinationBatches));
-                currentDestinationBatches.Clear();
-                payloadSize = 0;
+                // Add message to the current batch with a unique batch ID
+                currentBatch.Add(Guid.NewGuid().ToString(), message);
+
+                // Check if we've reached the maximum items per batch
+                if (currentBatch.Count == TransportConstraints.MaximumItemsInBatch)
+                {
+                    allBatches.Add(message.ToBatchRequest(currentBatch));
+                    currentBatch.Clear();
+                    currentBatchSize = 0;
+                }
             }
 
-            if (currentDestinationBatches.Count > 0)
+            // Finalize any remaining messages in the batch
+            if (currentBatch.Count > 0)
             {
-                allBatches.Add(firstMessage!.ToBatchRequest(currentDestinationBatches));
-                currentDestinationBatches.Clear();
+                allBatches.Add(referenceMessage!.ToBatchRequest(currentBatch));
+                currentBatch.Clear();
             }
         }
 
