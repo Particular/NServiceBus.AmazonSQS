eab251de39907bb72d95abad407451eff81d67be
Nick Gallegos <nick.gallegos@particular.net>
2025-05-22 16:00:46 -0600

    Got the console app working

--- src/NServiceBus.Transport.SQS.TestConsole/Program.cs
+++ src/NServiceBus.Transport.SQS.TestConsole/Program.cs
@@ -2,15 +2,61 @@
 
 namespace NServiceBus.Transport.SQS.TestConsole;
 
+using Amazon;
+using Amazon.Runtime;
+using Amazon.SimpleNotificationService;
+using Amazon.SQS;
+using Microsoft.Extensions.Configuration;
+
 class Program
 {
     static async Task Main()
     {
-        var transport = new SqsTransport();
-        var hostSettings = new HostSettings("", "", new StartupDiagnosticEntries(), (s, exception, arg3) => { }, true);
-        ReceiveSettings[] receivers = [new ReceiveSettings("", new QueueAddress(""), true, false, "")];
-        string[] sendingAddresses = [""];
-        var infra = await transport.Initialize(hostSettings, receivers, sendingAddresses).ConfigureAwait(false);
-        await infra.Receivers[""].StartReceive().ConfigureAwait(false);
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+        var queueName = "orderplacedevent";
+        var sqsClient = new AmazonSQSClient(new BasicAWSCredentials(config["Aws:AccessKey"], config["Aws:SecretAccessKey"]), RegionEndpoint.GetBySystemName(config["Aws:Region"]));
+        var snsClient = new AmazonSimpleNotificationServiceClient(new BasicAWSCredentials(config["Aws:AccessKey"], config["Aws:SecretAccessKey"]), RegionEndpoint.GetBySystemName(config["Aws:Region"]));
+        var transport = new SqsTransport(sqsClient, snsClient, true);
+        var hostSettings = new HostSettings(queueName, $"SQS Test Console for {queueName}", new StartupDiagnosticEntries(), (_, _, _) => { }, true, null);
+        ReceiveSettings[] receivers = [new ReceiveSettings(queueName, new QueueAddress(queueName), false, false, $"{queueName}_error")];
+        string[] sendingAddresses = [queueName];
+
+        var cts = new CancellationTokenSource();
+
+        var infra = await transport.Initialize(hostSettings, receivers, sendingAddresses, cts.Token).ConfigureAwait(false);
+        var receiver = infra.Receivers[queueName];
+        await receiver.Initialize(new PushRuntimeSettings(), OnMessage, OnError, cancellationToken: cts.Token).ConfigureAwait(false);
+        await receiver.StartReceive(cts.Token).ConfigureAwait(false);
+        // await Task.Run(async () =>
+        // {
+        //     await receiver.StartReceive(cts.Token).ConfigureAwait(false);
+        //     // while (!cts.IsCancellationRequested)
+        //     // {
+        //     //     await Task.Delay(100, cts.Token).ConfigureAwait(false);
+        //     //     await infra.Receivers[queueName].StartReceive(cts.Token).ConfigureAwait(false);
+        //     // }
+        // }, cts.Token).ConfigureAwait(false);
+
+        Console.ReadKey();
+
+        await cts.CancelAsync().ConfigureAwait(false);
+
+        Console.ReadKey();
+    }
+
+    static Task<ErrorHandleResult> OnError(ErrorContext errorcontext, CancellationToken cancellationtoken)
+    {
+        Console.WriteLine(errorcontext.Exception.Message);
+        return Task.FromResult(ErrorHandleResult.Handled);
+    }
+
+    static Task OnMessage(MessageContext messagecontext, CancellationToken cancellationtoken)
+    {
+        Console.WriteLine("received");
+        return Task.CompletedTask;
     }
 }
